ec8c67ddd66ed076071f8def02a68efd3c9bcb14
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-01-28 08:04:56 -0600

    New commit

---
+++ IdeaProjects/ConBadThreads/src/BadThreads.java
@@ -0,0 +1,34 @@
+public class BadThreads {
+
+    static String message;
+
+    private static class CorrectorThread extends Thread {
+
+        public void run() {
+            try {
+                sleep(1000);
+
+            } catch (InterruptedException e) {
+            }
+            message = "Mares do not eat oats";
+        }
+    }
+    public static void main (String[]args) throws InterruptedException {
+        (new CorrectorThread()).start();
+            message = "Mares do not eat oats";
+            Thread.sleep(2000);
+        System.out.println(message);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
